dd9cc918cc6e246ad6a329a9e0b873c2cf8026c1
atchu stonepaw <atchu.stonepaw@gmail.com@47290364-b148-ee83-9f4c-34a67cefbac1>
2011-07-03 03:56:49 +0000

    part of 1.7.2

--- losettings.py
+++ losettings.py
@@ -65,7 +65,7 @@ class settings:
 
 		self.Seperator = {"Characters" : "", "Genre" : "", "Tags" : "", "Teams" : "", "Writer" : "", "ScanInformation" : ""}
 
-		self.IllegalCharacters = {"?" : "", "/" : "", "\\" : "", "*" : "", ":" : "-", "<" : "[", ">" : "]", "|" : "!", "\"" : "'"}
+		self.IllegalCharacters = {"?" : "", "/" : "", "\\" : "", "*" : "", ":" : " - ", "<" : "[", ">" : "]", "|" : "!", "\"" : "'"}
 
 		self.Months = {1 : "January", 2 : "February", 3 : "March", 4 : "April", 5 : "May", 6 : "June", 7 : "July", 8 :"August", 9 : "September", 10 : "October",
 						11 : "November", 12 : "December"}
