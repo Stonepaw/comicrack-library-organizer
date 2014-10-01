f89ade9e0474df6d784b4b9fdff64d466c2fa792
Andrew Feltham <andrew.feltham3@gmail.com>
2014-09-30 22:59:17 -0600

    Still fixing the repo

---
+++ Stonepaw.LibraryOrganizer/GlobalSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Stonepaw.LibraryOrganizer
+{
+    class GlobalSettings
+    {
+        public string Version;
+        public List<string> LastUsedProfiles = new List<string>();
+        public bool GenerateLocalizedMonths = true;
+        public bool RemoveEmptyFolders = true;
+        public List<string> ExcludedEmptyFolders = new List<string>();
+        public Dictionary<int,string> MonthNames = new Dictionary<int,string>();
+        public Dictionary<string,string> IllegalCharacterReplacements = new Dictionary<string,string>();
+    }
+}
