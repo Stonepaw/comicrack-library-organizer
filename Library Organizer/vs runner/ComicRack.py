11182dbf2bab8acdaa8781c82cdd4c093245304e
Andrew Feltham <andrew.feltham3@gmail.com>
2014-10-01 10:48:44 -0600

    Still figuring out the hg repo

--- Library Organizer/vs runner/ComicRack.py
+++ Library Organizer/vs runner/ComicRack.py
@@ -1,3 +1,8 @@
+"""
+This module provides several classes that mock several methods provided by
+ComicRack so that the script can be run in Visual Studio.
+"""
+
 import System
 from System.Collections.Generic import Dictionary
 from System.IO import FileStream, FileMode, FileInfo, Path
@@ -9,20 +14,27 @@ class ComicRack(object):
         self.App = App()
 
     def Localize(self, resource, key, text):
+        """The Localize function would normally lookup a translated string
+        from a resource via a key. The text variable would be returned if
+        the key is not found in the resource. This mock function will
+        always return the text.
+        """
         return text
-
+    a
 class App(object):
-    """This is a replacement for methods provided by comicrack. This allows developing the program in Visual Studio without running CR."""
+    """This is a replacement for methods provided by ComicRack. 
+    This allows developing the program in Visual Studio without running CR."""
     
 
     def GetComicFields(self):
         return Dictionary[str, str]({"Tags " : "Tags","File Path " : "FilePath","Book Age " : "BookAge","Book Condition " : "BookCondition","Book Store " : "BookStore","Book Owner " : "BookOwner","Book Collection Status " : "BookCollectionStatus","Book Notes " : "BookNotes","Book Location " : "BookLocation","ISBN " : "ISBN","Title " : "Title","Series " : "Series","Number " : "Number","Alternate Series " : "AlternateSeries","Alternate Number " : "AlternateNumber","Story Arc " : "StoryArc","Series Group " : "SeriesGroup","Summary " : "Summary","Notes " : "Notes","Review " : "Review","Writer " : "Writer","Penciller " : "Penciller","Inker " : "Inker","Colorist " : "Colorist","Letterer " : "Letterer","Cover Artist " : "CoverArtist","Editor " : "Editor","Publisher " : "Publisher","Imprint " : "Imprint","Genre " : "Genre","Web " : "Web","Format " : "Format","Age Rating " : "AgeRating","Characters " : "Characters","Teams " : "Teams","Main Character Or Team " : "MainCharacterOrTeam","Locations " : "Locations","Scan Information " : "ScanInformation",})
 
     def GetLibraryBooks(self):
-        """ Mocks calling ComicRack's built in funtion to retrieve all
+        """ Mocks calling ComicRack's built in function to retrieve all
         the library books. It uses a file sample_data.dat which
-        is dumped from ComicRack using BinaryFormatter"""
-        with FileStream(Path.Combine(FileInfo(__file__).DirectoryName, "sample data.dat"), FileMode.Open) as f:
+        is dumped from ComicRack using a BinaryFormatter"""
+        with FileStream(Path.Combine(FileInfo(__file__).DirectoryName,
+                                     "sample data.dat"), FileMode.Open) as f:
             b = BinaryFormatter()
 
             return b.Deserialize(f)
