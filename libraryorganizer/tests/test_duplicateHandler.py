5fbef549728521bbecb4d77f9c3dde77b204388d
Andrew Feltham <andrew.feltham3@gmail.com>
2016-05-22 23:33:04 -0700

    Created more duplicate test cases

--- libraryorganizer/tests/test_duplicateHandler.py
+++ libraryorganizer/tests/test_duplicateHandler.py
@@ -1,11 +1,12 @@
 from System.IO import File, FileInfo
-import comicracknlogtarget
+
+import i18n
 from ComicRack import ComicRack
 from bookandprofiletestcase import BookAndProfileTestCase
-from bookmanager import DuplicateHandler
-from common import MoveFailedException, BookToMove
+from common import MoveFailedException, BookToMove, MoveSkippedException
+from duplicatehandler import DuplicateHandler
 from movereporter import MoveReporter
-import i18n
+
 i18n.setup(ComicRack())
 from duplicatewindow import DuplicateResult, DuplicateAction
 
@@ -18,40 +19,26 @@ class TestDuplicateHandler(BookAndProfileTestCase):
     def test__create_rename_path(self):
         """ This tests the create rename path function that rename paths are
         created correctly when several exist."""
-        f1 = self.create_temp_path("test (1).txt")
-        f2 = self.create_temp_path("test (2).txt")
-        File.Create(f1).Close()
-        File.Create(f2).Close()
-        self.cleanup_file_paths.append(f1)
-        self.cleanup_file_paths.append(f2)
+        f1 = self._create_temp_file("test (1).txt")
+        f2 = self._create_temp_file("test (2).txt")
         path = self.create_temp_path("test.txt")
-
         new_path = self.handler._create_rename_path(path)
         self.assertEqual(new_path, self.create_temp_path("test (3).txt"))
 
     def test__create_rename_path_already_duplicate(self):
         """ This tests the create rename path function that rename paths are
-        created correctly when several exist."""
-        f = self.create_temp_path("test.txt")
-        f1 = self.create_temp_path("test (1).txt")
-        f2 = self.create_temp_path("test (2).txt")
-        File.Create(f).Close()
-        File.Create(f1).Close()
-        File.Create(f2).Close()
-        self.cleanup_file_paths.append(f1)
-        self.cleanup_file_paths.append(f2)
-        self.cleanup_file_paths.append(f)
+        created correctly when several exist and the current path is already a duplicate."""
+        f = self._create_temp_file("test.txt")
+        f1 = self._create_temp_file("test (1).txt")
+        f2 = self._create_temp_file("test (2).txt")
         path = self.create_temp_path("test (1).txt")
-
         new_path = self.handler._create_rename_path(path)
         self.assertEqual(new_path, self.create_temp_path("test (3).txt"))
 
     def test__delete_duplicate(self):
         """ This tests that the duplicate delete code works as expected in a
         normal usage."""
-        dup_path = self.create_temp_path("test duplicate.txt")
-        File.Create(dup_path).Close()
-        self.cleanup_file_paths.append(dup_path)
+        dup_path = self._create_temp_file("test duplicate.txt")
         self.handler._delete_duplicate(dup_path)
         self.assertFalse(File.Exists(dup_path))
 
@@ -67,7 +54,19 @@ class TestDuplicateHandler(BookAndProfileTestCase):
         f1.Close()
 
     def test__find_book_in_library(self):
-        self.fail()
+        """ Tests that finding a real book in the library returns the book if on exists
+        and return None if it does not.
+
+        Tests that if there is a slight capitialization difference, the existing books is
+        still returned.
+
+        """
+        self.assertIsNotNone(self.handler._find_book_in_library(
+            'G:\\Comics\\DC Comics\\Adventure Comics (2009 Series)\\Adventure Comics Vol.2009 #01 (October, 2009).cbz'))
+        self.assertIsNotNone(self.handler._find_book_in_library(
+            'G:\\Comics\\DC Comics\\Adventure Comics (2009 Series)\\adventure comics Vol.2009 #01 (October, 2009).cbz'))
+        self.assertIsNone(self.handler._find_book_in_library(
+            'G:\\Comics\\DC Comics\\Adventure Comics (2009 Series)\\Adventure Comics Vol.2009 #19 (October, 2009).cbz'))
 
     def test_handle_duplicate_book(self):
         self.fail()
@@ -75,15 +74,14 @@ class TestDuplicateHandler(BookAndProfileTestCase):
     def test__process_different_extension_books_all_overwrite(self):
         def return_overwrite(*args):
             return DuplicateResult(DuplicateAction.Overwrite, False)
+
         self.handler._ask_user = return_overwrite
         f1 = self.create_temp_path("lotest.cbz")
-        f2 = self.create_temp_path("lotest.cbr")
-        f3 = self.create_temp_path("lotest.cbt")
-        File.Create(f2).Close()
-        File.Create(f3).Close()
+        f2 = self._create_temp_file("lotest.cbr")
+        f3 = self._create_temp_file("lotest.cbt")
         file2 = FileInfo(f2)
         file3 = FileInfo(f3)
-        self.cleanup_file_paths.extend([f1,f2,f3])
+        self.cleanup_file_paths.append(f1)
         b = BookToMove(self.book, f1, 1, None)
         b.duplicate_different_extension = True
         b.duplicate_ext_files = [file2, file3]
@@ -108,3 +106,7 @@ class TestDuplicateHandler(BookAndProfileTestCase):
 
     def test_remove_book_from_library(self):
         self.fail()
+
+    def test__cancelled(self):
+        with (self.assertRaises(MoveSkippedException)):
+            self.handler._cancelled(BookToMove(self.book, "a path", 1, []))
